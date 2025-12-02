8e21dfcd674b9c3969aa39a1d37accb44c338cc0
QingsongXu123 <108061884+QingsongXu123@users.noreply.github.com>
2025-12-02 15:26:58 +0100

    Add files via upload

--- PCNO/2D_Kolmogorov/experiments_test_PCNO.py
+++ PCNO/2D_Kolmogorov/experiments_test_PCNO.py
@@ -1,8 +1,11 @@
 import datetime
 import os
 import random
+import numpy as np
+import torch.nn.functional as F
 
 os.environ["CUDA_VISIBLE_DEVICES"]="0" # Select the available GPU
+from models.FNO import FNO2d
 from models.PCNO import PCNO2d
 
 from PIL import Image
@@ -12,7 +15,6 @@ import matplotlib.pyplot as plt
 from utils import pde_data, LpLoss, eq_check_rt, eq_check_rf
 
 import scipy
-import numpy as np
 from timeit import default_timer
 import argparse
 from torch.utils.tensorboard import SummaryWriter
@@ -51,10 +53,11 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument("--results_path", type=str, default="/Path_to/results/PCNO/NS/", help="path to store results")
 parser.add_argument("--suffix", type=str, default="seed1", help="suffix to add to the results path")
-parser.add_argument("--txt_suffix", type=str, default="ns_V1e-3_N10000_T30_PCNO_seed1", help="suffix to add to the results txt")
+parser.add_argument("--txt_suffix", type=str, default="ns_V1e-3_N10000_T30_PCNO_Test_seed1", help="suffix to add to the results txt")
 parser.add_argument("--data_path", type=str, default='/Path_to/Dataset/NS/ns_data4training_zli_samplefreq2e3_dsfreq4.h5', help="path to the data")
 parser.add_argument("--super", type=str, default='False', help="enable superres testing")
 parser.add_argument("--verbose",type=str, default='True')
+
 
 parser.add_argument("--T", type=int, default=20, help="number of timesteps to predict")
 parser.add_argument("--ntrain", type=int, default=1000, help="training sample size")
@@ -96,7 +99,7 @@ assert args.model_type in ["PCNO2d", "FNO2d",
                            "Ghybrid2d_p4", "Ghybrid2d_p4m",
                            "radialNO2d_p4", "radialNO2d_p4m",
                            "radialNO3d_p4", "radialNO3d_p4m",
-                           "Unet_Rot2d", "Unet_Rot_M2d", "Unet_Rot_3D"], f"Invalid model type {args.model_type}"
+                           "UNet2d", "Unet_Rot_M2d", "Unet_Rot_3D"], f"Invalid model type {args.model_type}"
 assert args.strategy in ["teacher_forcing", "markov", "recurrent", "oneshot"], "Invalid training strategy"
 
 torch.manual_seed(args.seed)
@@ -187,12 +190,10 @@ scheduler_gamma = args.gamma # for step scheduler
 
 initial_step = 1 if args.strategy == "markov" else T_in
 
-root = args.results_path + f"/{'_'.join(str(datetime.datetime.now()).split())}"
-if args.suffix:
-    root += "_" + args.suffix
-os.makedirs(root)
+root = '/Path_to_pre_trained/Models/2D_Kolmogorov/PCNO'
 path_model = os.path.join(root, 'model.pt')
 writer = SummaryWriter(root)
+
 
 ################################################################
 # Model init
@@ -225,50 +226,11 @@ elif "Ghybrid2d" in args.model_type:
     reflection = "p4m" in args.model_type
     model = Ghybrid2d(num_channels=num_channels, initial_step=initial_step, modes=modes, Gwidth=args.Gwidth,
                       width=width, reflection=reflection, n_equiv=args.n_equiv).cuda()
-elif "radialNO2d" in args.model_type:
-    reflection = "p4m" in args.model_type
-    model = radialNO2d(num_channels=num_channels, initial_step=initial_step, modes=modes, width=width, reflection=reflection,
-                       grid_type=grid_type).cuda()
-elif "radialNO3d" in args.model_type:
-    reflection = "p4m" in args.model_type
-    model = radialNO3d(num_channels=num_channels, initial_step=initial_step, modes=modes, time_modes=time_modes,
-                       width=width, reflection=reflection, grid_type=grid_type, time_pad=args.time_pad).cuda()
-elif args.model_type == "Unet_Rot2d":
-    model = Unet_Rot(input_frames=initial_step * num_channels, output_frames=num_channels, kernel_size=3, N=4).cuda()
-elif args.model_type == "Unet_Rot_M2d":
-    model = Unet_Rot_M(input_frames=initial_step * num_channels, output_frames=num_channels, kernel_size=3, N=4, grid_type=grid_type, width=width).cuda()
-elif args.model_type == "Unet_Rot_3D":
-    model = Unet_Rot_3D(input_frames=initial_step * num_channels, output_frames=num_channels, kernel_size=3, N=4, grid_type=grid_type, width=width).cuda()
 else:
     raise NotImplementedError("Model not recognized")
 
-# test model on training res and superres data
-if args.strategy == "oneshot":
-    x_shape = [batch_size, Sy, Sx, T, initial_step, num_channels]
-    x_shape_super = [1, S_super, S_super, T_super, initial_step, num_channels]
-elif args.strategy == "markov":
-    x_shape = [batch_size, Sy, Sx, 1, num_channels]
-    x_shape_super = [1, *(S_super, ) * d, 1, num_channels]
-else: # strategy == recurrent or teacher_forcing
-    x_shape = [batch_size, Sy, Sx, T_in, num_channels]
-    x_shape_super = [1, *(S_super, ) * d, T_in, num_channels]
 
-model.train()
-x = torch.randn(*x_shape).cuda()
-if args.strategy == "recurrent":
-    for _ in range(T):
-        im = model(x)
-        x = torch.cat([x[..., 1:, :], im], dim=-2)
-else:
-    model(x)
-# eq_check_rt(model, x, spatial_dims)
-# eq_check_rf(model, x, spatial_dims)
-# if args.super:
-#     model.eval()
-#     with torch.no_grad():
-#         x = torch.randn(*x_shape_super).cuda()
-#         model(x)
-
+model.load_state_dict(torch.load(path_model))
 ################################################################
 # load data
 ################################################################
@@ -411,7 +373,7 @@ test_rt_loader = torch.utils.data.DataLoader(test_rt_data, batch_size=batch_size
 test_rf_loader = torch.utils.data.DataLoader(test_rf_data, batch_size=batch_size, shuffle=False)
 
 ################################################################
-# training and evaluation
+# Inferance
 ################################################################
 
 complex_ct = sum(par.numel() * (1 + par.is_complex()) for par in model.parameters())
@@ -434,135 +396,6 @@ lploss = LpLoss(size_average=False)
 
 best_valid = float("inf")
 
-x_train, y_train = next(iter(train_loader))
-x = x_train.cuda()
-y = y_train.cuda()
-x_valid, y_valid = next(iter(valid_loader))
-if args.verbose:
-    print(f"{args.model_type}; Input shape: {x.shape}, Target shape: {y.shape}")
-if args.strategy == "oneshot":
-    assert x_train[0].shape == torch.Size([Sy, Sx, T, T_in, num_channels]), x_train[0].shape
-    assert y_train[0].shape == torch.Size([Sy, Sx, T, num_channels]), y_train[0].shape
-    assert x_valid[0].shape == torch.Size([Sy, Sx, T, T_in, num_channels]), x_valid[0].shape
-    assert y_valid[0].shape == torch.Size([Sy, Sx, T, num_channels]), y_valid[0].shape
-elif args.strategy == "markov":
-    assert x_train[0].shape == torch.Size([Sy, Sx, 1, num_channels]), x_train[0].shape
-    assert y_train[0].shape == torch.Size([Sy, Sx, num_channels]), y_train[0].shape
-    assert x_valid[0].shape == torch.Size([Sy, Sx, 1, num_channels]), x_valid[0].shape
-    assert y_valid[0].shape == torch.Size([Sy, Sx, T, num_channels]), y_valid[0].shape
-else: # strategy == recurrent or teacher_forcing
-    assert x_train[0].shape == torch.Size([Sy, Sx, T_in, num_channels]), x_train[0].shape
-    assert x_valid[0].shape == torch.Size([Sy, Sx, T_in, num_channels]), x_valid[0].shape
-    assert y_valid[0].shape == torch.Size([Sy, Sx, T, num_channels]), y_valid[0].shape
-    if args.strategy == "recurrent":
-        assert y_train[0].shape == torch.Size([Sy, Sx, T, num_channels]), y_train[0].shape
-    else: # strategy == teacher_forcing
-        assert y_train[0].shape == torch.Size([Sy, Sx, num_channels]), y_train[0].shape
-
-model.eval()
-# if args.verbose:
-#     print(f"{args.model_type} pre-train equivariance checks: Rotations - {eq_check_rt(model, x, spatial_dims)}, Reflections - {eq_check_rf(model, x, spatial_dims)}")
-start = default_timer()
-if args.verbose:
-    print("Training...")
-step_ct = 0
-train_times = []
-eval_times = []
-for ep in range(epochs):
-    model.train()
-    t1 = default_timer()
-
-    train_l2 = train_vort_l2 = train_pres_l2 = 0
-
-    for xx, yy in tqdm(train_loader, disable=not args.verbose):
-        loss = 0
-        xx = xx.cuda()
-        yy = yy.cuda()
-
-        if args.strategy == "recurrent":
-            for t in range(yy.shape[-2]):
-                y = yy[..., t, :]
-                # print('xx', xx.shape)
-                im = model(xx)
-                loss += lploss(im.reshape(len(im), -1, num_channels), y.reshape(len(y), -1, num_channels))
-                xx = torch.cat((xx[..., 1:, :], im), dim=-2)
-            loss /= yy.shape[-2]
-        else:
-            im = model(xx)
-            if args.strategy == "oneshot":
-                im = im.squeeze(-1)
-            # print('im',im.shape)
-            # print('yy', yy.shape)
-            loss = lploss(im.reshape(len(im), -1, num_channels), yy.reshape(len(yy), -1, num_channels))
-
-        train_l2 += loss.item()
-        if swe:
-            train_vort_l2 += lploss(im[..., VORT_IND].reshape(len(im), -1, 1), yy[..., VORT_IND].reshape(len(yy), -1, 1)).item()
-            train_pres_l2 += lploss(im[..., PRES_IND].reshape(len(im), -1, 1), yy[..., PRES_IND].reshape(len(yy), -1, 1)).item()
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-        if not args.step:
-            scheduler.step()
-        writer.add_scalar("Train/LR", scheduler.get_last_lr()[0], step_ct)
-        step_ct += 1
-    if args.step:
-        scheduler.step()
-
-    train_times.append(default_timer() - t1)
-
-    # validation
-    valid_l2 = valid_vort_l2 = valid_pres_l2 = 0
-    valid_loss_by_channel = None
-    with torch.no_grad():
-        model.eval()
-        model(xx)
-        for xx, yy in valid_loader:
-
-            xx = xx.cuda()
-            yy = yy.cuda()
-
-            # if valid_l2 == 0:
-            #     writer.add_scalar("Valid/Rotation", eq_check_rt(model, xx, spatial_dims), ep)
-            #     writer.add_scalar("Valid/Reflection", eq_check_rf(model, xx, spatial_dims), ep)
-
-            pred = get_eval_pred(model=model, x=xx, strategy=args.strategy, T=T, times=eval_times).view(len(xx), Sy, Sx, T, num_channels)
-
-            valid_l2 += lploss(pred.reshape(len(pred), -1, num_channels), yy.reshape(len(yy), -1, num_channels)).item()
-            if swe:
-                valid_vort_l2 += lploss(pred[..., VORT_IND].reshape(len(pred), -1, 1), yy[..., VORT_IND].reshape(len(yy), -1, 1)).item()
-                valid_pres_l2 += lploss(pred[..., PRES_IND].reshape(len(pred), -1, 1), yy[..., PRES_IND].reshape(len(yy), -1, 1)).item()
-
-    t2 = default_timer()
-    if args.verbose:
-        print(f"Ep: {ep}, time: {t2 - t1}, train: {train_l2 / ntrain}, valid: {valid_l2 / nvalid}")
-
-    writer.add_scalar("Train/Loss", train_l2 / ntrain, ep)
-    writer.add_scalar("Valid/Loss", valid_l2 / nvalid, ep)
-    if swe:
-        writer.add_scalar("Train Vorticity/Loss", train_vort_l2 / ntrain, ep)
-        writer.add_scalar("Train Pressure/Loss", train_pres_l2 / ntrain, ep)
-        writer.add_scalar("Valid Vorticity/Loss", valid_vort_l2 / nvalid, ep)
-        writer.add_scalar("Valid Pressure/Loss", valid_pres_l2 / nvalid, ep)
-
-    if valid_l2 < best_valid:
-        best_epoch = ep
-        best_valid = valid_l2
-        torch.save(model.state_dict(), path_model)
-    # if args.early_stopping:
-    #     if ep - best_epoch > args.early_stopping:
-    #         break
-
-stop = default_timer()
-train_time = stop - start
-train_times = torch.tensor(train_times).mean().item()
-num_eval = len(eval_times)
-eval_times = torch.tensor(eval_times).mean().item()
-model.eval()
-# if args.verbose:
-#     print(f"{args.model_type} post-train equivariance checks: Rotations - {eq_check_rt(model, xx, spatial_dims)}, Reflections - {eq_check_rf(model, xx, spatial_dims)}")
-
-# test
 model.load_state_dict(torch.load(path_model))
 model.eval()
 test_l2 = test_vort_l2 = test_pres_l2 = 0
@@ -578,69 +411,10 @@ with torch.no_grad():
         pred = get_eval_pred(model=model, x=xx, strategy=args.strategy, T=T, times=[]).view(len(xx), Sy, Sx, T, num_channels)
         test_l2 += lploss(pred.reshape(len(pred), -1, num_channels), yy.reshape(len(yy), -1, num_channels)).item()
 
-        if swe:
-            test_vort_l2 += lploss(pred[..., VORT_IND].reshape(len(pred), -1, 1), yy[..., VORT_IND].reshape(len(yy), -1, 1)).item()
-            test_pres_l2 += lploss(pred[..., PRES_IND].reshape(len(pred), -1, 1), yy[..., PRES_IND].reshape(len(yy), -1, 1)).item()
-
-        rotations_l2 += lploss(model(xx).rot90(dims=list(spatial_dims)[:2]).reshape(len(pred), -1, num_channels),
-                              model(xx.rot90(dims=list(spatial_dims)[:2])).reshape(len(pred), -1, num_channels))
-        reflections_l2 += lploss(model(xx).flip(dims=(spatial_dims[0], )).reshape(len(pred), -1, num_channels),
-                                model(xx.flip(dims=(spatial_dims[0], ))).reshape(len(pred), -1, num_channels))
-
-    for xx, yy in test_rt_loader:
-        xx = xx.cuda()
-        yy = yy.cuda()
-        pred = get_eval_pred(model=model, x=xx, strategy=args.strategy, T=T, times=[]).view(len(xx), Sy, Sx, T, num_channels)
-        test_rt_l2 += lploss(pred.reshape(len(pred), -1, num_channels), yy.reshape(len(yy), -1, num_channels)).item()
-
-    for xx, yy in test_rf_loader:
-        xx = xx.cuda()
-        yy = yy.cuda()
-        pred = get_eval_pred(model=model, x=xx, strategy=args.strategy, T=T, times=[]).view(len(xx), Sy, Sx, T, num_channels)
-        test_rf_l2 += lploss(pred.reshape(len(pred), -1, num_channels), yy.reshape(len(yy), -1, num_channels)).item()
-    rotations_l2 = rotations_l2 / ntest
-    reflections_l2 = reflections_l2 / ntest
-    writer.add_scalar("Test/Rotation", test_rt_l2 / ntest, best_epoch)
-    writer.add_scalar("Test/Reflection", test_rf_l2 / ntest, best_epoch)
     writer.add_scalar("Test/Loss", test_l2 / ntest, best_epoch)
-    if swe:
-        writer.add_scalar("Test Vorticity/Loss", test_vort_l2 / ntest, best_epoch)
-        writer.add_scalar("Test Pressure/Loss", test_pres_l2 / ntest, best_epoch)
-
+    
 test_time_l2 = test_space_l2 = ntest_super = test_int_space_l2 = test_int_time_l2 = None
-
 print(f"{args.model_type} done training; \nTest: {test_l2 / ntest}, Rotations: {rotations_l2}, Reflections: {reflections_l2}, Super Space Test: {test_space_l2}, Super Time Test: {test_time_l2}")
-summary = f"Args: {str(args)}" \
-          f"\nParameters: {complex_ct}" \
-          f"\nTrain time: {train_time}" \
-          f"\nMean epoch time: {train_times}" \
-          f"\nMean inference time: {eval_times}" \
-          f"\nNum inferences: {num_eval}" \
-          f"\nTrain: {train_l2 / ntrain}" \
-          f"\nValid: {valid_l2 / nvalid}" \
-          f"\nTest: {test_l2 / ntest}" \
-          f"\nRotation Test: {test_rt_l2 / ntest}" \
-          f"\nReflection Test: {test_rf_l2 / ntest}" \
-          f"\nSuper Space Test: {test_space_l2}" \
-          f"\nSuper Space Interpolation Test: {test_int_space_l2}" \
-          f"\nSuper S: {S_super}" \
-          f"\nSuper Time Test: {test_time_l2}" \
-          f"\nSuper Time Interpolation Test: {test_int_time_l2}" \
-          f"\nSuper T: {T_super}" \
-          f"\nBest Valid: {best_valid / nvalid}" \
-          f"\nBest epoch: {best_epoch + 1}" \
-          f"\nTest Rotation Equivariance loss: {rotations_l2}" \
-          f"\nTest Reflection Equivariance loss: {reflections_l2}" \
-          f"\nEpochs trained: {ep}"
-if swe:
-    summary += f"\nVorticity Test: {test_vort_l2 / ntest}" \
-               f"\nPressure Test: {test_pres_l2 / ntest}"
-txt = "results"
-if args.txt_suffix:
-    txt += f"_{args.txt_suffix}"
-txt += ".txt"
 
-with open(os.path.join(root, txt), 'w') as f:
-    f.write(summary)
 writer.flush()
 writer.close()
